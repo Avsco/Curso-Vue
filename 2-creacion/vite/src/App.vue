00f539c9e543e9e7f5fb3585a42628151ba2d633
Alan Benavides <bruno@coastt.org>
2021-09-08 21:28:33 -0400

    Segunda Clase

---
+++ 2-creacion/vite/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="center">
+		<p>{{ count }}</p>
+		<button @click="addCount">add</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				count: 1,
+				boolean: true,
+			};
+		},
+		methods: {
+			addCount() {
+				this.count++;
+			},
+			changeBool() {
+				this.boolean != this.boolean;
+			},
+		},
+	};
+</script>
+
+<style>
+	.center {
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+</style>
