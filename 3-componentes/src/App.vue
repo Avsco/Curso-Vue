7d96bed0594fff1e477d86810379fa2f1d90f335
Alan Benavides <bruno@coastt.org>
2021-09-13 15:26:21 -0400

    add: Tercera clase

---
+++ 3-componentes/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="center">
+		<counter></counter>
+		<Counter :propNumber="1" />
+	</div>
+</template>
+
+<script>
+	import Counter from "./components/Counter.vue";
+
+	export default {
+		components: {
+			Counter,
+		},
+		created() {
+			console.log("Inyectado JS");
+		},
+		mounted() {
+			console.log("Montado el HTML");
+		},
+		beforeDestroy() {},
+	};
+</script>
+
+<style>
+	.center {
+		min-height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+
+	body {
+		margin: 0;
+	}
+</style>
---
+++ 3-componentes/src/components/Counter.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		<h1>{{ pow2 }}</h1>
+		<button @click="add">add</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		// props: ["number"],  no recomendado
+		props: {
+			propNumber: {
+				type: Number,
+				required: true,
+				default: 4,
+			},
+		},
+		data() {
+			return {
+				number: this.propNumber,
+			};
+		},
+		created() {
+			console.log("Inyectado JS HIJO");
+		},
+		methods: {
+			add() {
+				this.number++;
+			},
+		},
+		computed: {
+			pow2() {
+				return this.number * this.number;
+			},
+		},
+		watch: {
+			number(newValue, oldValue) {
+				if (newValue > 5) {
+					this.number = 5;
+				}
+			},
+		},
+	};
+</script>
